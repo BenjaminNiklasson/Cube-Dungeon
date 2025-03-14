f5bd537e63d768e7962315ff20eabb43d61a8833
BenjaminNiklasson <benjamin.niklasson@elev.ga.lbs.se>
2025-03-14 14:00:54 +0100

    sss

---
+++ Dungeon Crawl/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 moveInput;
+    Rigidbody2D rb;
+    [SerializeField] float moveSpeed = 3f;
+    [SerializeField] float rotateSpeed = 2000f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = moveInput * moveSpeed;
+
+        if (moveInput != Vector2.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, moveInput);
+            Quaternion rotate = Quaternion.RotateTowards(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+
+            rb.MoveRotation(rotate);
+        }
+        else
+        {
+            rb.angularVelocity = 0;
+        }
+    }
+}
