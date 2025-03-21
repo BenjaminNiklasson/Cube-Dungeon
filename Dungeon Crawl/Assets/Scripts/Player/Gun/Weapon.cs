4eee6c627212fd74f006e483c0d29c1de4b219c4
Marcus-Larsson <Mackan2.larsson2008@gmail.com>
2025-03-21 15:17:52 +0100

    Guncooldown

--- Dungeon Crawl/Assets/Scripts/Player/Gun/Weapon.cs
+++ Dungeon Crawl/Assets/Scripts/Player/Gun/Weapon.cs
@@ -7,10 +7,13 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     [SerializeField] float playerBulletSpeed = 10f;
-    //[SerializeField] float playerShootingSpeed = 10f;
-    //[SerializeField] float playerWeaponCooldown = 10f;
+    [SerializeField] float playerDamage = 10f;
+    [SerializeField] float playerWeaponCooldown = 0.5f;
     [SerializeField] GameObject playerBullet;
     [SerializeField] GameObject playerGun;
+
+    private float nextFireTime = 0f;
+    bool hasFired;
 
     //bool fasterBullets = false;
     //bool lessCooldown = false;
@@ -19,7 +22,7 @@ public class Weapon : MonoBehaviour
     //bool spreadShot = false;
     //bool pierceBullet = false;
     //bool bulletLifeSteal = false;
-   
+
 
     void Start()
     {
@@ -28,12 +31,24 @@ public class Weapon : MonoBehaviour
 
     void OnFire()
     {
-        GameObject bullet = Instantiate(playerBullet, playerGun.transform.position, transform.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(transform.up * playerBulletSpeed, ForceMode2D.Impulse);
+        if (!hasFired)
+        {
+            GameObject bullet = Instantiate(playerBullet, playerGun.transform.position, transform.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(transform.up * playerBulletSpeed, ForceMode2D.Impulse);
+            hasFired = true;
+        }
     }
     void Update()
     {
-        
+        if (hasFired)
+        {
+            nextFireTime += Time.deltaTime;
+            if (nextFireTime > playerWeaponCooldown)
+            {
+                hasFired = false;
+                nextFireTime = 0;
+            }
+        }
     }
 }
