2f7ac2d649bf58eaf3f10f33394e26f08a28c6b2
NilasAlbinson <nilas.albinson@gmail.com>
2025-03-27 16:36:55 +0100

    I took away temporary weapon

--- Dungeon Crawl/Assets/Scripts/Player/Gun/Weapon.cs
+++ Dungeon Crawl/Assets/Scripts/Player/Gun/Weapon.cs
@@ -11,29 +11,47 @@ public class Weapon : MonoBehaviour
     //[SerializeField] float playerWeaponCooldown = 10f;
     [SerializeField] GameObject playerBullet;
     [SerializeField] GameObject playerGun;
+    float bulletSpreadDegrees = 15;
+    int numberOfBullets = 5;
 
     //bool fasterBullets = false;
     //bool lessCooldown = false;
     //bool moreDamage = false;
     //bool bounceBullet = false;
-    //bool spreadShot = false;
+    bool spreadShot = false;
     //bool pierceBullet = false;
     //bool bulletLifeSteal = false;
-   
+
 
     void Start()
     {
-        
+        //spreadShot = true;
     }
 
     void OnFire()
     {
-        GameObject bullet = Instantiate(playerBullet, playerGun.transform.position, transform.rotation);
-        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-        rb.AddForce(transform.up * playerBulletSpeed, ForceMode2D.Impulse);
+        if (spreadShot)
+        {
+            for (int i = 0; i < numberOfBullets; i++)
+            {
+                float baseRotationZ = transform.rotation.eulerAngles.z;
+                float randomOffset = Random.Range(bulletSpreadDegrees, -bulletSpreadDegrees);
+                Quaternion bulletSpawnRotation = Quaternion.Euler(0, 0, baseRotationZ + randomOffset);
+                GameObject bullet = Instantiate(playerBullet, playerGun.transform.position, bulletSpawnRotation);
+                Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+                rb.AddForce(bullet.transform.up * playerBulletSpeed, ForceMode2D.Impulse);
+            }
+        }
+        else
+        {
+            GameObject bullet = Instantiate(playerBullet, playerGun.transform.position, transform.rotation);
+            Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(transform.up * playerBulletSpeed, ForceMode2D.Impulse);
+        }
     }
     void Update()
     {
-        
+
     }
 }
+
