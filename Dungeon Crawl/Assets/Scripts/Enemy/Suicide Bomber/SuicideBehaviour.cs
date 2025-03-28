2da3fb2d1930757df03360c9460ac69e58415661
BenjaminNiklasson <benjamin.niklasson@elev.ga.lbs.se>
2025-03-28 13:01:05 +0100

    balls

---
+++ Dungeon Crawl/Assets/Scripts/Enemy/Suicide Bomber/SuicideBehaviour.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuicideBehaviour : MonoBehaviour
+{
+    Rigidbody2D rb;
+    GameObject player;
+    [SerializeField] float eSpeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 direction = (player.transform.position - transform.position).normalized;
+        rb.MovePosition(rb.position + direction * eSpeed * Time.fixedDeltaTime);
+    }
+}
