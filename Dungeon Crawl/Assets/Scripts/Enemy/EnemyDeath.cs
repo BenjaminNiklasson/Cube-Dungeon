29e157a1860311e4f4bec2b69b98388970dd36d1
BenjaminNiklasson <benjamin.niklasson@elev.ga.lbs.se>
2025-03-28 13:33:08 +0100

    sss

--- Dungeon Crawl/Assets/Scripts/Enemy/EnemyDeath.cs
+++ Dungeon Crawl/Assets/Scripts/Enemy/EnemyDeath.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyDeath : MonoBehaviour
 {
+    public int health;
+    [SerializeField] string enemyType;
+
     // Start is called before the first frame update
     void Start()
     {
