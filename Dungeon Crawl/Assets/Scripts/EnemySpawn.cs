f2e382a85f854db52ba8be4846b88a6e8c91246f
BenjaminNiklasson <benjamin.niklasson@elev.ga.lbs.se>
2025-04-06 10:02:38 +0200

    fixed the spawn system
    (crashes computer though)

--- Dungeon Crawl/Assets/Scripts/EnemySpawn.cs
+++ Dungeon Crawl/Assets/Scripts/EnemySpawn.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Xml.Serialization;
 using UnityEngine;
 
@@ -10,31 +12,73 @@ public class EnemySpawn : MonoBehaviour
     [SerializeField] List<int> wave2 = new List<int>();
     [SerializeField] List<int> wave3 = new List<int>();
     [SerializeField] float spawnCooldown = 1f;
+    [SerializeField] GameObject swarmer;
+    [SerializeField] GameObject shooter;
+    [SerializeField] GameObject sewerslider;
     [SerializeField] GameObject jeff;
-    [SerializeField] GameObject Swarmer;
-    [SerializeField] GameObject Shooter;
-    [SerializeField] GameObject Sewerslider;
-
+    List<int> activeWave;
+    bool spawnStarted = false;
+    bool enemiesLeft = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        waves.Add(wave1);
+        waves.Add(wave2);
+        waves.Add(wave3);
+        activeWave = waves[Random.Range(0, waves.Count)];
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Invoke("Spawn", spawnCooldown);
+        enemiesLeft = false;
+        for (int i = 0; i<activeWave.Count; i++)
+        {
+            if (activeWave[i] != 0)
+            {
+                enemiesLeft = true;
+            }
+        }
+        if (spawnStarted == false && enemiesLeft == true)
+        {
+            spawnStarted = true;
+            UnityEngine.Debug.Log("Entered");
+            Invoke("Spawn", spawnCooldown);
+        }
     }
     void Spawn()
     {
+        int enemiesToSpawn = 0;
+        int enemyType = 0;
         Transform spawnPoint = transform.GetChild(Random.Range(0, transform.childCount));
-
+        while (enemiesToSpawn == 0)
+        {
+            
+            enemyType = Random.Range(0, 3);
+            enemiesToSpawn = activeWave[enemyType];
+        }
+        switch(enemyType)
+        {
+            case 0:
+                Instantiate(swarmer, spawnPoint);
+                break;
+            case 1:
+                Instantiate(shooter, spawnPoint);
+                break;
+            case 2:
+                Instantiate(sewerslider, spawnPoint);
+                break;
+            case 3:
+                Instantiate(jeff, spawnPoint);
+                break;
+        }
+        activeWave[enemyType]--;
+        enemiesLeft = false;
+        for (int i = 0; i<activeWave.Count; i++)
+        {
+            if (activeWave[i] != 0)
+            {
+                enemiesLeft = true;
+            }
+        }
         Invoke("Spawn", spawnCooldown);
     }
 }
