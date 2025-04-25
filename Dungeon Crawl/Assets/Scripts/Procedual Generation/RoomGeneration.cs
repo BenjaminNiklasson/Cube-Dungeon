88b4b59ded62582a8b824f009fae5d4bc8a1d6fc
BenjaminNiklasson <benjamin.niklasson@elev.ga.lbs.se>
2025-04-25 14:42:34 +0200

    sss

--- Dungeon Crawl/Assets/Scripts/Procedual Generation/RoomGeneration.cs
+++ Dungeon Crawl/Assets/Scripts/Procedual Generation/RoomGeneration.cs
@@ -16,7 +16,7 @@ public class RoomGeneration : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        sortRooms();
     }
 
     // Update is called once per frame
