6f6262bac4d72d25e7286ba0817577877e7169e0
NilasAlbinson <nilas.albinson@gmail.com>
2025-05-09 14:51:13 +0200

    fgdghdfsad

--- Dungeon Crawl/Assets/Scripts/Procedual Generation/RoomSpawnpoint.cs
+++ Dungeon Crawl/Assets/Scripts/Procedual Generation/RoomSpawnpoint.cs
@@ -1,7 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class RoomSpawnpoint : MonoBehaviour
 {
     List<bool> _roomDoors = new List<bool>();
@@ -9,7 +11,7 @@ public class RoomSpawnpoint : MonoBehaviour
     int _roomNumber;
     Collider2D _doorTrigger;
 
-    public RoomSpawnpointNumber(int number, Transform position)
+    public RoomSpawnpoint(int number, Transform position)
     {
         _roomNumber = number;
         _roomPosition = position;
