6f8dec7c5219da965a88ff751d3cf8d97c90fd38
Marcus-Larsson <Mackan2.larsson2008@gmail.com>
2025-05-23 13:25:06 +0200

    ExitSpawn

---
+++ Dungeon Crawl/Assets/Scripts/ExitSpawn.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitSpawn : MonoBehaviour
+{
+    public bool iSpawn = true;
+    public string prefabName = "ExitSpawn";
+    public Vector3 spawnPosition = Vector3.zero;
+    [SerializeField] GameObject exitSpawn;
+
+    void Start()
+    {
+        
+    }
+
+    void SpawnPrefab()
+    {
+        
+        if (exitSpawn != null)
+        {
+            Instantiate(exitSpawn, spawnPosition, Quaternion.identity);
+        }
+        else
+        {
+            Debug.LogError("Prefab not found in Resources folder!");
+        }
+    }
+
+    void Update()
+    {
+        if (iSpawn == true)
+        {
+            SpawnPrefab();
+        }
+    }
+    
+    //private void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (other.CompareTag("Player")) // Check the incoming object
+    //    {
+    //        Debug.Log("Player entered the area!");
+    //        SceneManager.LoadScene(SceneManager.GetActiveScene().name); // Reload current scene
+    //    }
+    //}
+    
+
+}
